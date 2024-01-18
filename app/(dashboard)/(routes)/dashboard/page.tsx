98b4502da7f785964ecbbf9c813036ed8d033548
Faotu Happy <79374710+Faotu@users.noreply.github.com>
2024-01-18 08:14:53 +0100

    sign-up and sign-in config using clerk

---
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -0,0 +1,5 @@
+const DashboardPage = () => {
+  return <p>Dashboad Page (This is a protected page)</p>;
+};
+
+export default DashboardPage;
---
+++ app/(main)/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const HomePage = () => {
+  return <div>Home Page (Unprotected page)</div>;
+};
+
+export default HomePage;
